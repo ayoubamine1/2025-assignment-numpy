b0f6f9a91d9bb3c5b009a624be14a2703288dd99
Ayoub <ayoub.amine@emines.um6p.ma>
2025-12-15 14:23:17 +0100

    Numpy Assignment

--- numpy_questions.py
+++ numpy_questions.py
@@ -39,9 +39,7 @@ def max_index(X):
     """
     i = 0
     j = 0
-
-    # TODO
-
+    i, j = np.unravel_index(np.argmax(X), X.shape)
     return i, j
 
 
@@ -62,6 +60,10 @@ def wallis_product(n_terms):
     pi : float
         The approximation of order `n_terms` of pi using the Wallis product.
     """
-    # XXX : The n_terms is an int that corresponds to the number of
-    # terms in the product. For example 10000.
-    return 0.
+    if not isinstance(n_terms, int) or n_terms < 0:
+        raise ValueError("n_terms must be a non-negative integer")
+    product = 1.
+    for i in range(1, n_terms + 1):
+        product *= (4 * i**2) / (4 * i**2 - 1)
+
+    return 2 * product
